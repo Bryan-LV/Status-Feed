f0af79d4657328410f4e776c08d6d464c5bea793
Bryan Almaraz <bryan.almaraz96@gmail.com>
2020-01-28 14:15:45 +0100

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -14,6 +15,16 @@ connectDB();
 // Routes
 app.use('/api/user', require('./routes/user'));
 app.use('/api/feed', require('./routes/feed'));
+
+// Server static assets in production
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 4000;
 
